ef2bb9b042f7bca6865021e937985f6b4b337849
unknown <secours@a-018834.ups.u-psud.fr>
2025-05-06 15:27:57 +0200

    distance

--- src/point.cpp
+++ src/point.cpp
@@ -1,3 +1,5 @@
 #include "point.hpp"
-
+#include cmaths
 Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+double distance(Point B = Point()){
+	return(sqrt((A.y-B.y)*(A.y-B.y)+(A.x-B.x)*(A.x-B.x));} 
