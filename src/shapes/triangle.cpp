d802e09f9008c861f9a2a418d1e2377868315a16
unknown <secours@a-018854.ups.u-psud.fr>
2025-05-06 16:19:30 +0200

    MESSAGE

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -1,7 +1,9 @@
 #include "point.hpp"
 #include "shapes/triangle.hpp"
+#include "draw.hpp"
 #include <cmath>
 using namespace std;
+#include <vector>
 
 Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {};
 double Triangle::perimeter(){
@@ -19,4 +21,8 @@ Point Triangle::center(){
 	G.x=(A.x+B.x+C.x)/3;
 	G.y=(A.y+B.y+C.y)/3;
 	return G;
+}
+void Triangle::draw(){
+	vector<Point> V = {A,B,C};
+	draw_picture(V);
 }
