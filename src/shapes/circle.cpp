aaecb4cd32608bffbc658aafc0c0739e0ad1097a
unknown <secours@a-018849.ups.u-psud.fr>
2025-05-06 15:13:39 +0200

    definition of the functions circumference and area

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -1,4 +1,18 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
+#include <cmath>
+using namespace std;
 
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
+double Circle::circumference{
+	return 2*M_pi*radius;
+}
+
+double Circle::area{
+	return r*r*M_pi;
+}
+
+double Circle::draw{
+	return 0;
+}
