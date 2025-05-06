9f50066a395f3522a90bd758196266daa3816584
unknown <secours@a-018854.ups.u-psud.fr>
2025-05-06 15:40:10 +0200

    MESSAGE

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -1,4 +1,22 @@
 #include "point.hpp"
 #include "shapes/triangle.hpp"
+#include <cmath>
+using namespace std;
 
-Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {}
+Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {};
+double Triangle::perimeter(){
+	return A.distance(B) + B.distance(C) + C.distance(A);
+}
+double Triangle::area(){
+	double AB=A.distance(B);
+	double BC=B.distance(C);
+	double CA=C.distance(A);
+	double P=(AB+BC+CA)/2;
+	return sqrt(P*(P-AB)*(P-BC)*(P-CA));
+}
+Point Triangle::center(){
+	Point G;
+	G.x=(A.x+B.x+C.x)/3;
+	G.y=(A.y+B.y+C.y)/3;
+	return G;
+}
