2486ad7f92fc5cd1c96edf6a4501c441f23ef08c
pierre-asom <pierre-andreas.asomaning@etu-upsaclay.fr>
2025-05-06 16:25:44 +0200

    added side

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,8 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
 
-Square::Square(Point P, Point R) : A(P), C(R) {};
+Square::Square(Point P, Point R){
+	A=P;
+	C=R;
+double Square::side(){
+		return(P.distance(R)/sqrt(2))
