4a4a7df43b221fe89f6aa1f1ff220917b3f7cd96
pierre-asom <pierre-andreas.asomaning@etu-upsaclay.fr>
2025-05-12 15:05:06 +0200

    draw_square

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,6 +1,8 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
 #include <cmath>
+#include <vector>
+#include "draw.hpp"
 
 Square::Square(Point P, Point R){
 	A=P;
@@ -13,3 +15,10 @@ double Square::area(){
 return side()*side();}
 Point Square::center(){
 	return Point((A.x+C.x)/2,(A.y+C.y)/2);}
+void Square::draw(){
+std::vector<Point> points;
+points.at(0)=A;
+points.at(1)=Point(A.x, A.y-side());
+points.at(2)=C;
+points.at(3)=Point(C.x, C.y+side());
+draw_picture(points);}
