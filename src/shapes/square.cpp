a78f543a6c2850a2f83ac03380283e27650b8720
pierre-asom <pierre-andreas.asomaning@etu-upsaclay.fr>
2025-05-12 15:19:48 +0200

    draw

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -8,17 +8,21 @@ Square::Square(Point P, Point R){
 	A=P;
 	C=R;}
 double Square::side(){
-return(A.distance(C)/sqrt(2));}
+	return(A.distance(C)/sqrt(2));
+}
 double Square::perimeter(){
-return 4*side();}
+	return 4*side();
+}
 double Square::area(){
-return side()*side();}
+	return side()*side();
+}
 Point Square::center(){
-	return Point((A.x+C.x)/2,(A.y+C.y)/2);}
+	return Point((A.x+C.x)/2,(A.y+C.y)/2);
+}
 void Square::draw(){
-std::vector<Point> points;
-points.at(0)=A;
-points.at(1)=Point(A.x, A.y-side());
-points.at(2)=C;
-points.at(3)=Point(C.x, C.y+side());
-draw_picture(points);}
+	std::vector<Point> points;
+	points.push_back(A);
+	points.push_back(Point(A.x, A.y-side()));
+	points.push_back(C);
+	points.push_back(Point(C.x, C.y+side()));
+	draw_picture(points);}
